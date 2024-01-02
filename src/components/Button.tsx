1a6ceb0f81381e63468a7c560ca281474b44a83e
Anton Stihl <anton.stihl@nordnet.se>
2024-01-02 15:18:41 +0100

    make button shadows smaller

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,18 +4,25 @@ interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   variant?: Variant;
 }
 
-export default function Button({ variant = 'secondary', ...props }: ButtonProps) {
+export default function Button({
+  variant = "secondary",
+  ...props
+}: ButtonProps) {
   const colorStyle =
     variant === "primary"
       ? "bg-green-300"
       : variant === "secondary"
       ? "bg-white"
       : "";
-  const hoverColorStyle =
-    "hover:" + colorStyle.slice(0, colorStyle.length - 3) + "400";
+  const hoverColorStyle = colorStyle.slice(0, colorStyle.length - 3) + "400";
+  const shadowStyle =
+    variant === "primary"
+      ? "shadow-black shadow-md active:shadow-sm active:shadow-black"
+      : "shadow-black shadow-sm";
+
   return (
     <button
-      className={`${colorStyle} rounded-md border-black px-2 py-1 ${hoverColorStyle} active:shadow-sm active:shadow-black shadow-md shadow-black flex items-center justify-center ${
+      className={`${colorStyle} rounded-md border-black px-2 py-1 hover:${hoverColorStyle} ${shadowStyle} flex items-center justify-center ${
         props.disabled ? "cursor-not-allowed" : ""
       }`}
       {...props}
