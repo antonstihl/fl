f4f7ab0a8bdf4fbcf141a5cceb43dd7f7af7012f
Anton Stihl <anton.stihl@nordnet.se>
2024-01-10 21:59:14 +0100

    bar chart

--- src/pages/SalaryPage.tsx
+++ src/pages/SalaryPage.tsx
@@ -1,10 +1,19 @@
 import { useState } from "react";
+import {
+  Bar,
+  BarChart,
+  CartesianGrid,
+  Legend,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
 import Button from "../components/Button";
 import Card from "../components/Card";
 import { useParents } from "../context/ParentContext";
 import { useSalaries } from "../hooks/useSalaries";
 import { convertToDate, convertToMyDate } from "../utils/DateUtilities";
-import ConfirmModal from "../components/ConfirmModal";
+import { PRISBASBELOPP } from "../utils/Forsakringskassan";
 
 type SalaryToAdd = {
   id: string;
@@ -20,10 +29,8 @@ export default function SalaryPage() {
   const [salaryToAdd, setSalaryToAdd] = useState<SalaryToAdd>({
     id: crypto.randomUUID(),
   });
+  const [salaryDate, setSalaryDate] = useState<Date>(new Date());
   const { addSalary, deleteSalary, salaries } = useSalaries();
-  const [salaryToDelete, setSalaryToDelete] = useState<string | undefined>(
-    undefined
-  );
 
   const validateSalary = (s: SalaryToAdd): boolean => {
     if (s.employer && s.parentId && s.startDate && s.amountSEK) {
@@ -38,9 +45,31 @@ export default function SalaryPage() {
       setSalaryToAdd({ id: crypto.randomUUID() });
     }
   };
+
+  const pbb = PRISBASBELOPP.find(
+    (pb) => pb.year === salaryDate.getFullYear()
+  )?.amount;
+  const roof = pbb ? pbb * 10 : undefined;
+
+  const salaryGraphData = parents.map((p) => {
+    const parentYearlySalary = salaries
+      .filter((s) => s.parentId === p.id)
+      .filter(
+        (s) =>
+          convertToDate(s.startDate) <= salaryDate &&
+          (s.endDate ? convertToDate(s.endDate) >= salaryDate : true)
+      )
+      .reduce((a, b) => a + b.amountSEK, 0);
+    return {
+      name: p.name,
+      salaryBelowRoof: roof && Math.min(parentYearlySalary, roof),
+      salaryAboveRoof: roof && Math.max(parentYearlySalary - roof, 0),
+    };
+  });
   return (
+    <div className="flex gap-2 flex-wrap">
       <div className="m-4 flex flex-col items-start gap-2">
-        <Card>
+        <Card width="w-full">
           <div className="grid grid-cols-2 gap-2 p-2 items-center">
             <label htmlFor="employer">Arbetsgivare</label>
             <input
@@ -134,6 +163,53 @@ export default function SalaryPage() {
             </Button>
           </div>
         </Card>
+        <Card width="w-fit">
+          <div className="flex flex-col items-center">
+            <div className="flex justify-end w-full pr-8">
+              <input
+                className="border-2 border-black p-1"
+                type="date"
+                max={"3000-01-01"}
+                min={"1990-01-01"}
+                value={salaryDate.toLocaleDateString("sv-SE")}
+                onChange={(e) => setSalaryDate(new Date(e.target.value))}
+              />
+            </div>
+            <BarChart
+              width={500}
+              height={300}
+              data={salaryGraphData}
+              margin={{
+                top: 20,
+                right: 30,
+                left: 20,
+                bottom: 5,
+              }}
+            >
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="name" />
+              <YAxis />
+              <Tooltip />
+              <Legend />
+              <Bar
+                name={`Lön under FK tak (${
+                  roof ? roof / 1000 : "?"
+                }k år ${salaryDate.getFullYear()})`}
+                dataKey="salaryBelowRoof"
+                stackId="a"
+                fill="#8884d8"
+              />
+              <Bar
+                name="Lön över FK tak"
+                dataKey="salaryAboveRoof"
+                stackId="a"
+                fill="#82ca9d"
+              />
+            </BarChart>
+          </div>
+        </Card>
+      </div>
+      <div className="m-4 flex flex-col gap-2">
         <Card>
           {salaries.length === 0 ? (
             <p>No salaries saved.</p>
@@ -163,5 +239,6 @@ export default function SalaryPage() {
           )}
         </Card>
       </div>
+    </div>
   );
 }
---
+++ src/utils/Forsakringskassan.ts
@@ -0,0 +1,7 @@
+export const PRISBASBELOPP = [
+  { year: 2024, amount: 57300 },
+  { year: 2023, amount: 52500 },
+  { year: 2022, amount: 48300 },
+  { year: 2021, amount: 47600 },
+  { year: 2020, amount: 47300 },
+];
