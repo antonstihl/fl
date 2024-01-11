b0470d4a2e8b375165e47fb7d58888b111e5157d
Anton Stihl <anton.stihl@nordnet.se>
2024-01-11 21:08:17 +0100

    fix: unused import

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,6 @@ import App from "./App.tsx";
 import Help from "./components/Help.tsx";
 import CalendarPage from "./pages/CalendarPage.tsx";
 import FamilyPage from "./pages/FamilyPage.tsx";
-import StatsPage from "./pages/StatsPage.tsx";
 import SalaryPage from "./pages/SalaryPage.tsx";
 
 const landingLoader = async () => {
